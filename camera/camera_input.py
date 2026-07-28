e620fd028e0755f913ef06c88d00f5bb108451a2
charanram32975 <charanram32975@gmail.com>
2026-07-28 17:32:36 +0530

    Create camera_input.py

---
+++ camera/camera_input.py
@@ -0,0 +1,14 @@
+import cv2
+
+class CameraInput:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    def release(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
