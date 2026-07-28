3fe1f9f0eec855365641c33d764bf688a5790072
charanram32975 <charanram32975@gmail.com>
2026-07-28 17:35:59 +0530

    Create cnn_model.py

---
+++ model/cnn_model.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+class CNNModel:
+    def build_model(self, num_classes):
+        model = Sequential([
+            Conv2D(32, (3,3), activation='relu', input_shape=(64,64,1)),
+            MaxPooling2D(2,2),
+
+            Conv2D(64, (3,3), activation='relu'),
+            MaxPooling2D(2,2),
+
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dense(num_classes, activation='softmax')
+        ])
+
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        return model
